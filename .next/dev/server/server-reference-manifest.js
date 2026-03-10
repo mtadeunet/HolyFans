ad87e923d057508524a1b29e2b7d45f323c133f8
Miguel Tadeu <mtadeunet@gmail.com>
2026-03-10 21:20:46 +0000

    chore: remove .next build artifacts from git tracking
    - Remove all .next/ files from git tracking
    - .next/ already exists in .gitignore to prevent future tracking
    - Build artifacts should not be version controlled

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Cq3HIljPClPeRBnmvH0UKUb3/Whf812YEGFfyFZSXS0=\"\n}"
--- .next/dev/static/chunks/app_[locale]_page_tsx_27cfbaf0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_394a5f93._.js",
-  "static/chunks/node_modules_223386db._.js"
-],
-    source: "dynamic"
-});
